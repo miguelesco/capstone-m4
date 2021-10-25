26d6b58f2d6587ceccb7c054169c7e76d599bcf8
miguelesco <miuelangel1999@gmail.com>
2021-10-25 16:02:48 -0400

    change logic of main

---
+++ src/Classes/item.rb
@@ -0,0 +1,42 @@
+class Item
+  def initialize(publish_date, archived: false)
+    @id = Random.rand(1..10_000)
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_source(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+
+  def can_be_archived?
+    @publish_date > 10
+  end
+
+  def move_to_archive?
+    @archived = can_be_archived?
+  end
+end
+
+item1 = Item.new(9)
+item2 = Item.new(15)
+p item1
+p item2
+p item1.can_be_archived?
+p item1.move_to_archive?
