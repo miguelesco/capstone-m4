422dedf0d549928590ef9af34aa9e2c70648104b
miguelesco <miuelangel1999@gmail.com>
2021-10-25 17:01:13 -0400

    class and methods ready

--- src/Classes/item.rb
+++ src/Classes/item.rb
@@ -26,17 +26,10 @@ class Item
   end
 
   def can_be_archived?
-    @publish_date > 10
+    @publish_date.to_i > 10
   end
 
   def move_to_archive?
     @archived = can_be_archived?
   end
 end
-
-item1 = Item.new(9)
-item2 = Item.new(15)
-p item1
-p item2
-p item1.can_be_archived?
-p item1.move_to_archive?
