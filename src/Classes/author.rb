b8fd0ded9a4a6997df90ba4ddd773b3b542665ef
Arthur Guzman <alxguzmanc@gmail.com>
2021-10-25 15:34:03 -0500

    Fix rubocop issues in Author class

--- src/Classes/author.rb
+++ src/Classes/author.rb
@@ -1,14 +1,15 @@
 class Author
-    attr_accessor :id, :first_name, :last_name, :items
-    def initialize(id = Random.rand(1..10_000), first_name, last_name)
-        @id = id
-        @first_name = first_name
-        @last_name = last_name
-        @items = []
-    end
+  attr_accessor :id, :first_name, :last_name, :items
 
-    def add_item(item)
-        @items << item unless @items.include?(item)
-        item.author = self
-      end
+  def initialize(first_name, last_name, id = Random.rand(1..10_000))
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.author = self
+  end
 end
