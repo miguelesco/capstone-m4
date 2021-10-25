99759eca3724a6fabbdc3d55fb31d45699acc86d
Arthur Guzman <alxguzmanc@gmail.com>
2021-10-25 15:20:52 -0500

    Fix issues in Game class

--- src/Classes/game.rb
+++ src/Classes/game.rb
@@ -1,18 +1,15 @@
-require_relative './item.rb'
+require_relative './item'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
-    def initialize(multiplayer, last_played_at, publish_date)
-        super(publish_date)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  attr_accessor :multiplayer, :last_played_at
 
-    def can_be_archived?
-        super && @last_played_at > 2
-    end
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && @last_played_at > 2
+  end
 end
-
-game = Game.new('ewe', 3, 10)
-p game
-p game.can_be_archived?
