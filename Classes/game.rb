253b1405b9f28cbee080fc2ca509e8f545135016
Arthur Guzman <alxguzmanc@gmail.com>
2021-10-25 13:45:21 -0500

    Add attr_accessor to Game class

--- Classes/game.rb
+++ Classes/game.rb
@@ -1,5 +1,5 @@
 class Game
-
+    attr_accessor :multiplayer, :last_played_at
     def initialize(multiplayer, last_played_at)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
