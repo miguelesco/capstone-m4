6a797034cbdbd08c15c1b631f61fc6342a4bfa98
Arthur Guzman <alxguzmanc@gmail.com>
2021-10-26 12:38:56 -0500

    Add games table

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -71,3 +71,31 @@ CREATE TABLE movies(
       REFERENCES label(id)
       ON DELETE CASCADE
 );
+
+CREATE TABLE games(
+  id SERIAL PRIMARY KEY,
+  genre_id INT,
+  author_id INT,
+  source_id INT,
+  label_id INT,
+  publish_date date,
+  archived boolean,
+  multiplayer boolean,
+  last_played_at date,
+  CONSTRAINT genre_id
+    FOREIGN KEY(genre_id)
+      REFERENCES genre(id)
+      ON DELETE CASCADE,
+  CONSTRAINT source_id
+    FOREIGN KEY(source_id)
+      REFERENCES source(id)
+      ON DELETE CASCADE,
+  CONSTRAINT author_id
+    FOREIGN KEY(author_id)
+      REFERENCES author(id)
+      ON DELETE CASCADE,
+  CONSTRAINT label_id
+    FOREIGN KEY(label_id)
+      REFERENCES label(id)
+      ON DELETE CASCADE
+)
