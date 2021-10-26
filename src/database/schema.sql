b5d0579b955ff01ec9585789e9600d2e306c7e01
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-10-26 16:43:05 -0300

    Update music_album table

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -76,27 +76,7 @@ CREATE TABLE movies(
 CREATE TABLE music_album(
   id SERIAL PRIMARY KEY,
   on_spotify: boolean,
-  publish_date date,
-  archived boolean,
-  silent boolean,
-  genre_id INT,
-  source_id INT,
-  author_id INT,
-  label_id INT,
-  CONSTRAINT genre_id
-    FOREIGN KEY(genre_id)
-      REFERENCES genre(id)
-      ON DELETE CASCADE,
-  CONSTRAINT source_id
-    FOREIGN KEY(source_id)
-      REFERENCES source(id)
-      ON DELETE CASCADE,
-  CONSTRAINT author_id
-    FOREIGN KEY(author_id)
-      REFERENCES author(id)
-      ON DELETE CASCADE,
-  CONSTRAINT label_id
-    FOREIGN KEY(label_id)
-      REFERENCES label(id)
-      ON DELETE CASCADE
+  CONSTRAINT (
+    FOREIGN KEY(item_id) REFERENCES item(id)
+  )
 );
