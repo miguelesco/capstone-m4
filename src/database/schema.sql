bd3d5ae511038b5156ef9c64058244c24e93131b
Arthur Guzman <alxguzmanc@gmail.com>
2021-10-26 14:52:39 -0500

    Fix schema game table 2.0

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -74,7 +74,28 @@ CREATE TABLE movies(
 
 CREATE TABLE games(
   id SERIAL PRIMARY KEY,
+  genre_id INT,
+  author_id INT,
+  source_id INT,
+  label_id INT,
+  publish_date date,
+  archived boolean,
   multiplayer boolean,
   last_played_at date,
-  FOREIGN KEY(item_id) REFERENCES item(id)
+  CONSTRAINT genre_id
+    FOREIGN KEY(genre_id)
+      REFERENCES genre(id)
+      ON DELETE CASCADE,
+  CONSTRAINT source_id
+    FOREIGN KEY(source_id)
+      REFERENCES source(id)
+      ON DELETE CASCADE,
+  CONSTRAINT author_id
+    FOREIGN KEY(author_id)
+      REFERENCES author(id)
+      ON DELETE CASCADE,
+  CONSTRAINT label_id
+    FOREIGN KEY(label_id)
+      REFERENCES label(id)
+      ON DELETE CASCADE
 )
