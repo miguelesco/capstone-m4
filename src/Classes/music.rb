683ab5cf8818772afb45573eaab0892b9b6daae0
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-10-26 14:24:58 -0300

    Create unit tests for MusicAlbum class

--- src/Classes/music.rb
+++ src/Classes/music.rb
@@ -1,6 +1,8 @@
 require_relative 'item'
 
 class MusicAlbum < Item
+  attr_accessor :publish_date, :on_spotify
+  
   def initialize(publish_date, on_spotify)
     super(publish_date)
     @on_spotify = on_spotify
